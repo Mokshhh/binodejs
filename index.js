683f916439f1f910694df28d18b39ced6958219d
Mokshhh <62876843+Mokshhh@users.noreply.github.com>
2022-03-01 01:54:02 +0530

    public folder created

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(express.static('public'));  
 
 
-app.get('/index', function (req, res) {  
-   res.sendFile( __dirname + "/" + "index.html" );  
-})  
-
 
 app.post('/process_post', urlencodedParser, function (req, res) {  
-   return res.redirect('/user')  
+   response = {  
+      first_name:req.body.first_name,  
+      last_name:req.body.last_name  
+  };  
+  console.log(response);  
+  res.end(JSON.stringify(response)); 
 })  
 
 
