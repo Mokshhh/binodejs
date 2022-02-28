44818b7934c304ad621b3a7cce2ba9850eba0847
Mokshhh <62876843+Mokshhh@users.noreply.github.com>
2022-02-28 23:04:23 +0530

    push

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 var express = require('express');  
 var app = express();  
-app.get('/index', function (req, res) {  
-    res.sendFile( __dirname + "/" + "index.html" );  
- }) 
- 
- app.post('/process_post',  function (req, res) {  
-    response = {  
-        first_name:req.body.first_name,  
-        last_name:req.body.last_name  
-    };  
-    console.log(response);  
-    res.end(JSON.stringify(response));  
- })  
+var bodyParser = require('body-parser');  
 
- var server = app.listen(8000, function () {  
-    var host = server.address().address  
-    var port = server.address().port  
-    console.log("Example app listening at http://%s:%s", host, port)  
-  })  
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })  
+app.use(express.static('public'));  
+
+
+app.get('/index', function (req, res) {  
+   res.sendFile( __dirname + "/" + "index.html" );  
+})  
+
+
+app.post('/process_post', urlencodedParser, function (req, res) {  
+   response = {  
+       first_name:req.body.first_name,  
+       last_name:req.body.last_name  
+   };  
+   console.log(response);  
+   res.end(JSON.stringify(response));  
+})  
+
+
+var server = app.listen(8000, function () {  
+  var host = server.address().address  
+  var port = server.address().port  
+  console.log("Example app listening at $port", host, port)  
+})  
+
