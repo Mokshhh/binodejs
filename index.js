fd2ffc323b9f50edf2b470f3e2dca52d9bbe237b
Mokshhh <62876843+Mokshhh@users.noreply.github.com>
2022-03-03 20:06:54 +0530

    added features

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
 var express = require('express');  
+// for http module
+var http = require('http');
 var app = express();  
-var bodyParser = require('body-parser');  
+const port = 3000;
+// The simplest way to read a JSON file is to require it. Passing require() with the path to a JSON file will synchronously read and parse the data into a JavaScript object.
 
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false })  
+var urlencodedParser = express.urlencoded() 
+const fs = require("fs");
 app.use(express.static('public'));  
+//function dosent work when imported (need fixing)
+function desponse (req, res) {  
+  response = {  
+     first_name:req.body.first_name,  
+     last_name:req.body.last_name , 
+     password:req.body.password
+ };  res.end(JSON.stringify(response)); 
+//
+//  code the fs lines insie the function only.
+//  
+ const jsonString = JSON.stringify(response);
+ fs.writeFileSync('userdata.json', jsonString);
+};
+
+app.post('/userdata', urlencodedParser, desponse);
+
+//current stage: in the server 8000, form takes the data and sends it to /userdata
+//next stage: /userdata sends the data to userdata.json
+//output shows error that res is not defined
+// fs.readFile('/userdata', function(err, data) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write(data);
+//   return res.end(JSON.stringify(response));
+// });
 
 
+// this function will read the /userdata webpage and show it in userdata.json
+// function userdatas (req, res) {
+//   fs.readFile('/userdata', function(err, data) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     return res.end();
+//   });
+// }
 
-app.post('/userdata', urlencodedParser, function (req, res) {  
-   response = {  
-      first_name:req.body.first_name,  
-      last_name:req.body.last_name  
-  };  
-  console.log(response);  
-  res.end(JSON.stringify(response)); 
-})  
+//note to self: objects in function cant be used outside that function
+//console.log('The resulted',jsonString);
 
-
-var server = app.listen(8000, function () {  
-  var host = server.address().address  
-  var port = server.address().port  
+app.listen(port, function () {  
   console.log(`Example app listening at ${port}`)  
-})  
-
+})
